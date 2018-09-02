6447718fef702f48cd77dda24c0d5a4dfcc44ea6
Daren Schwenke <darenschwenke@gmail.com>
2018-09-01 20:52:39 -0400

    Scripted part rendering

--- render_all.sh
+++ render_all.sh
@@ -7,15 +7,18 @@ do
 			for depth in 4 8 12 16
 			do
 				echo "Rendering ${part}_${length}u-${depth}mm_depth.stl"
-				openscad -o rendered/${part}_${length}u-${depth}.stl -D 'print="base"' -D "block_l=${length}" -D "tape_d=${depth}" P1-Drag_tape_feeder.scad
+				openscad -o rendered/${part}_${length}u-${depth}mm_depth.stl -D "print='${part}'" -D "length=${length}" -D "depth=${depth}" P1-Drag_tape_feeder.scad
 			done
 		else
 			for width in 8
 			do
 		
 				echo "Rendering ${part}_${length}u-${width}mm_width.stl"
-				openscad -o rendered/${part}_${length}u-${depth}.stl -D 'print="base"' -D "block_l=${length}" -D "tape_w=${width}" P1-Drag_tape_feeder.scad
+				openscad -o rendered/${part}_${length}u-${width}mm_width.stl -D "print='${part}'" -D "length=${length}" -D "width=${width}" P1-Drag_tape_feeder.scad
 			done
 		fi
 	done
 done
+git add *
+git commit -m "Scripted part rendering"
+git push
